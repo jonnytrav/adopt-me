cf4ebda25e1834caaa447814a4e88b3a53d2f257
Jonathan Traverso <jonathan.traverso@withrove.com>
2022-03-11 21:20:09 -0500

    Carousel, context theme provier, dynamic component routes, custom useBreed hook.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 /* global React */
-import Pet from "./Pet.js";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { useState } from "react";
+import SearchParams from "./SearchParams.js";
+import Details from "./Details.js";
+import ThemeContext from "./ThemeContext.js";
 
 const App = () => {
-  return <Pet name="Sparky" animal="dog" breed="lab" />;
+  const theme = useState("darkblue");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,89 @@
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext.js";
+import useBreedList from "./useBreedList.js";
+import Results from "./Results.js";
+
+const ANIMALS = ["bird", "dog", "cat", "snake"];
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [theme] = useContext(ThemeContext);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    console.log(json);
+
+    setPets(json.pets);
+  }
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="location"
+          />
         </label>
-        <button>Submit</button>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
